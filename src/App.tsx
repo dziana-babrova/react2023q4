a5c7e1fed04c91a2acd9ac35b8402f1cd3bc44bb
Diana_Bobrova <dziana_babrova@epam.com>
2023-10-26 22:03:52 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css';
 import Header from './components/header/header';
+import { Footer } from './components/footer/footer';
 import { Wrapper } from './components/wrapper/wrapper';
 
 function App() {
   return (
-    <Wrapper>
-      <Header></Header>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Header></Header>
+      </Wrapper>
+      <Wrapper>
+        <Footer></Footer>
+      </Wrapper>
+    </>
   );
 }
 

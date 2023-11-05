4ecea05b18a6e0d60a6af9c07848fb262257c36a
Diana_Bobrova <dziana_babrova@epam.com>
2023-11-06 01:14:36 +0300

    feat: add details

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -1,7 +1,56 @@
 import { useDetailedInfo } from 'hooks/useDetailedInfo';
 import './details.scss';
+import { Loader } from 'components/loader/loader';
+import { ErrorMessage } from 'components/error-message/error-message';
+import { useNavigate } from 'react-router-dom';
 
 export const Details = () => {
-  const [data] = useDetailedInfo();
-  return <div className="details">{data?.result.title}</div>;
+  const [data, isLoading, hasError] = useDetailedInfo();
+  const navigate = useNavigate();
+
+  const closeDetails = () => {
+    console.log('close');
+    navigate(-1);
+  };
+
+  return (
+    <div className="details">
+      <div className="overlay" onClick={closeDetails}></div>
+      <div className="details-content">
+        {isLoading && <Loader></Loader>}
+        {hasError && (
+          <ErrorMessage text="Oops... An error occurred. Please try again later"></ErrorMessage>
+        )}
+        {data?.result && (
+          <>
+            <img className="details-image" src={data.result.image}></img>
+            <p className="details-title">{data.result.title}</p>
+            <div className="details-info">
+              <div className="details-info-item">
+                Country:{' '}
+                <p className="details-info-content">{data.result.country}</p>
+              </div>
+              <div className="details-info-item">
+                Description:{' '}
+                <p className="details-info-content">
+                  {data.result.description}
+                </p>
+              </div>
+              <div className="details-info-item">
+                Year: <p className="details-info-content">{data.result.year}</p>
+              </div>
+              <div className="details-info-item">
+                Status:{' '}
+                <p className="details-info-content">{data.result.status}</p>
+              </div>
+              <div className="details-info-item">
+                Imdb rating:{' '}
+                <p className="details-info-content">{data.result.imdbRating}</p>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
